aad3e25365450c028a516a144e483cbd03d10d37
OEM Configuration (temporary user) <davidndy@gmail.com>
2018-01-03 20:11:50 -0500

    Challenge duties calculation search done.

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,14 @@
 class ExpensesController < ApplicationController
   def index
      @expenses = Expense.order("date DESC")
+     if params[:concept].present?
+        @expenses = @expenses.where("concept LIKE :query", query: "%#{params[:concept]}%")
+      end
+
+
+      
+      if (params[:category_id].present?)
+        @expenses = @expenses.where("category_id LIKE :query", query: "%#{params[:category_id]}%")
+      end
   end
 end
